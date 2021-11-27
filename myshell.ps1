5edb61ae9d2975809eef2e74592997aec243b800
gxhao <1625546088@qq.com>
2021-11-27 20:45:15 +0800

    修改软件地址为aiur仓库

--- myshell.ps1
+++ myshell.ps1
@@ -1,4 +1,7 @@
 winget list -e --id JetBrains.IntelliJIDEA.Ultimate --source winget
 
+## TODO  pin to start 
+## TODO  software seting
 
-iex ((New-Object System.Net.WebClient).DownloadString('https://githubcontent.aiurs.co/162554/configuration-script-win/raw/main/PROFILE.ps1'))
+
+$(New-WinUserLanguageList -Language en-US).Add("en-US")
