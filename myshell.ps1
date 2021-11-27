a80c5d046f96c7e90595b69bca3273bd794e322a
gxhao <1625546088@qq.com>
2021-11-27 20:20:16 +0800

    修改仓库地址

---
+++ myshell.ps1
@@ -0,0 +1,4 @@
+winget list -e --id JetBrains.IntelliJIDEA.Ultimate --source winget
+
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://githubcontent.aiurs.co/162554/configuration-script-win/raw/main/PROFILE.ps1'))
