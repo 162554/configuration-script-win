36d745f5be773a70fb57feb090668f8956366fb5
gxhao <1625546088@qq.com>
2021-11-27 18:24:51 +0800

    修改一些配置

--- install.ps1
+++ install.ps1
@@ -62,26 +62,14 @@ function Install-OtherSoftWare {
     param (
         [string]$softwareurl
     )
-    $sofearename = $softwareurl.split("/")[-1]
-    Write-Host "sofearename is  $($sofearename )" -ForegroundColor Green
-    $outpath = "$env:USERPROFILE\Downloads\$sofearename"
+    $softwarename = $softwareurl.split("/")[-1]
+    Write-Host "softwarename is  $($softwarename )" -ForegroundColor Green
+    $outpath = "$env:USERPROFILE\Downloads\$softwarename"
     $wc = New-Object System.Net.WebClient
     $wc.DownloadFile($softwareurl, $outpath)
-    Write-Host "Attempting to install: $sofearename..." -ForegroundColor Green
+    Start-Sleep -Seconds 10
+    Write-Host "Install-OtherSoftWare softwarename: $softwarename..." -ForegroundColor Green
     Start-Process -Filepath  $outpath  "/S"  -Wait 2>&1 | out-null
-    <# 无法判断是否已安装
-    $InstalledSoftware = Get-ChildItem "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
-    foreach($obj in $InstalledSoftware){
-        if($sofearename.Contains($obj.GetValue('DisplayName'))){
-            Write-Host "$sofearename is already installed!" -ForegroundColor Green
-            break
-        }
-        else {
-            Write-Host "Attempting to install: $sofearename..." -ForegroundColor Green
-            Start-Process -Filepath  $outpath  "/S"
-        }
-    }
-    #>
 }
 
 function Set-WallPaper($Image) {
@@ -112,25 +100,11 @@ Write-Host "-----------------------------" -ForegroundColor Green
 if (-not(Get-IsElevated)) { 
     throw "Please run this script as an administrator" 
 }
-
-# anduin 
-<#
-if (-not $(Get-Command Connect-AzureAD -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing Nuget PowerShell Package Provider..." -ForegroundColor Green
-    Install-PackageProvider -Name NuGet -Force
-    Write-Host "Installing AzureAD PowerShell module..." -ForegroundColor Green
-    Install-Module AzureAD -Force
-} else {
-    Write-Host "Azure AD PowerShell Module is already installed!" -ForegroundColor Green
-}
-#>
-#$aad = Connect-AzureAD
-#$email = $aad.Account.Id
-Write-Host "请先确保已登录微软账号和onedrive账号，" -ForegroundColor Yellow
-Write-Host "Press the [C] key to continue to steps which requires reboot."
+Write-Host "Please make sure you have logged in to Microsoft Accounts and OneDrive."
+Write-Host "Press the [y] key to continue to steps which requires reboot."
 $pressedKey = Read-Host
 Write-Host "You pressed: $($pressedKey)"
-if ($pressedKey -eq 'c') {
+if ($pressedKey -eq 'y') {
     Write-Host "Please provide me your email." -ForegroundColor Yellow
     $email = Read-Host
     Write-Host "Please provide me your name." -ForegroundColor Yellow
@@ -160,7 +134,7 @@ if ($pressedKey -eq 'c') {
     $wmiObj.UpdateScanMethod() | Format-Table -AutoSize
 
 
-    ## Reimage 后应该先更新商店再安装winget，否则找不到winget包
+    ## Reimage 
     # Install Winget
     if (-not $(Get-Command winget -ErrorAction SilentlyContinue)) {
         Write-Host "Installing WinGet..." -ForegroundColor Green
@@ -170,8 +144,6 @@ if ($pressedKey -eq 'c') {
             Start-Sleep -Seconds 5
         }
     }
-
-
     if ("$(winget list --id Microsoft.VisualStudioCode --source winget)".Contains("--")) { 
         Write-Host "Microsoft.VisualStudioCode is already installed!" -ForegroundColor Green
     }
@@ -184,30 +156,35 @@ if ($pressedKey -eq 'c') {
     #Install-IfNotInstalled "Microsoft.Teams"
     #Install-IfNotInstalled "Microsoft.Office"
     Install-IfNotInstalled "Microsoft.OneDrive"
-    Install-IfNotInstalled "Microsoft.PowerShell"
+    #Install-IfNotInstalled "Microsoft.PowerShell"
     #Install-IfNotInstalled "Microsoft.dotnet"
     ## Java
-    Install-IfNotInstalled "JetBrains.IntelliJIDEA.Ultimate"
+    Install-IfNotInstalled "JetBrains.IntelliJIDEA.Ultimate"   #bug 无法判断是否已安装
     Install-IfNotInstalled "EclipseAdoptium.Temurin.11"
     ## Netease
     Install-IfNotInstalled "Netease.CloudMusic"
     Install-IfNotInstalled "Microsoft.Edge"
     Install-IfNotInstalled "Microsoft.EdgeWebView2Runtime"
-    Install-IfNotInstalled "Microsoft.AzureDataStudio"
+    #Install-IfNotInstalled "Microsoft.AzureDataStudio"
     ## Tencent
     Install-IfNotInstalled "Tencent.WeChat"
     Install-IfNotInstalled "Tencent.QQ"
-    Install-IfNotInstalled "SoftDeluxe.FreeDownloadManager"
-    Install-IfNotInstalled "VideoLAN.VLC"
-    Install-IfNotInstalled "OBSProject.OBSStudio"
+    #Install-IfNotInstalled "SoftDeluxe.FreeDownloadManager"
+    #Install-IfNotInstalled "VideoLAN.VLC"
+    #Install-IfNotInstalled "OBSProject.OBSStudio"
     Install-IfNotInstalled "Git.Git"
     Install-IfNotInstalled "OpenJS.NodeJS"
     Install-IfNotInstalled "Postman.Postman"
     #Install-IfNotInstalled "7zip.7zip"
-    Install-IfNotInstalled "CPUID.CPU-Z"
-    Install-IfNotInstalled "WinDirStat.WinDirStat"
-    Install-IfNotInstalled "FastCopy.FastCopy"
+    #Install-IfNotInstalled "CPUID.CPU-Z"
+    #Install-IfNotInstalled "WinDirStat.WinDirStat"
+    #Install-IfNotInstalled "FastCopy.FastCopy"
     #Install-IfNotInstalled "DBBrowserForSQLite.DBBrowserForSQLite"
+    Install-IfNotInstalled "Youdao.YoudaoNote"
+    Install-IfNotInstalled "ZeroTier.ZeroTierOne"
+    Install-IfNotInstalled "utools.utools"
+    Install-IfNotInstalled "Postman.Postman"
+    
 
     Install-StoreApp -storeAppId "9NBLGGH5R558" -wingetAppName "Microsoft To Do"
     Install-StoreApp -storeAppId "9MV0B5HZVK9Z" -wingetAppName "Xbox"
@@ -217,102 +194,31 @@ if ($pressedKey -eq 'c') {
     Install-StoreApp -storeAppId "9ncbcszsjrsb" -wingetAppName "Spotify Music"
     Install-StoreApp -storeAppId "9mspc6mp8fm4" -wingetAppName "Microsoft Whiteboard"
     Install-StoreApp -storeAppId "9wzdncrfhvjl" -wingetAppName "OneNote for Windows 10"
-    ## other 任务栏透明
     Install-StoreApp -storeAppId "9pf4kz2vn4w9" -wingetAppName "TranslucentTB"
 
-    Write-Host "Reloading environment variables..." -ForegroundColor Green
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-    ## 使用微软官方工具下载
-    Install-OtherSoftWare "https://res.u-tools.cn/version2/uTools-2.4.3.exe"
+    #Install-OtherSoftWare "https://res.u-tools.cn/version2/uTools-2.4.3.exe"
     Install-OtherSoftWare "https://www.mgnb.jp/download/MGNB.exe"
     Install-OtherSoftWare "https://github.com/AutoDarkMode/Windows-Auto-Night-Mode/releases/download/10.1.0.10/AutoDarkModeX_10.1.0.10.exe"
-    Install-OtherSoftWare "https://download.ydstatic.com/notewebsite/downloads/YNote.exe"
-    Install-OtherSoftWare "https://download.zerotier.com/dist/ZeroTierOne.msi"
+    #Install-OtherSoftWare "https://download.ydstatic.com/notewebsite/downloads/YNote.exe"
+    #Install-OtherSoftWare "https://download.zerotier.com/dist/ZeroTierOne.msi"
+    <#
+    Write-Host "Configuring FDM..." -ForegroundColor Green
+    cmd /c "taskkill.exe /IM fdm.exe /F"
+    Remove-Item -Path "$env:LOCALAPPDATA\Softdeluxe" -Force -Recurse -ErrorAction SilentlyContinue
+    Start-Process "$env:ProgramFiles\Softdeluxe\Free Download Manager\fdm.exe"
+    Start-Sleep -Seconds 5
+    cmd /c "taskkill.exe /IM fdm.exe /F"
+    $fdmDbPath = "$env:LOCALAPPDATA\Softdeluxe\Free Download Manager\db.sqlite"
+    Invoke-WebRequest -Uri "https://github.com/Anduin2017/configuration-script-win/raw/main/db.sqlite" -OutFile "$fdmDbPath"
+    #>
 
-
-    <# 不使用fdm下载
-Write-Host "Configuring FDM..." -ForegroundColor Green
-cmd /c "taskkill.exe /IM fdm.exe /F"
-Remove-Item -Path "$env:LOCALAPPDATA\Softdeluxe" -Force -Recurse -ErrorAction SilentlyContinue
-Start-Process "$env:ProgramFiles\Softdeluxe\Free Download Manager\fdm.exe"
-Start-Sleep -Seconds 5
-cmd /c "taskkill.exe /IM fdm.exe /F"
-$fdmDbPath = "$env:LOCALAPPDATA\Softdeluxe\Free Download Manager\db.sqlite"
-Invoke-WebRequest -Uri "https://github.com/Anduin2017/configuration-script-win/raw/main/db.sqlite" -OutFile "$fdmDbPath"
-#>
-    <#  不需要chromium FFmpeg 
-if ($true) { 
-    Write-Host "Installing Chromium as backup browser ..." -ForegroundColor Green
-    $chromiumUrl = "https://download-chromium.appspot.com/dl/Win_x64?type=snapshots"
-    $chromiumPath = "${env:ProgramFiles}\Chromium"
-    
-    $downloadedChromium = $env:USERPROFILE + "\Downloads\Win_x64.zip"
-    Remove-Item $downloadedChromium -ErrorAction SilentlyContinue
-    Start-Process "$env:ProgramFiles\Softdeluxe\Free Download Manager\fdm.exe" -PassThru "$chromiumUrl -force"
-        
-    while(-not $(Get-Item $downloadedChromium -ErrorAction SilentlyContinue))
-    {
-        Write-Host "Chromium is still not downloaded!"
-        Start-Sleep -Seconds 5
-    }
-    
-    Move-Item $downloadedChromium "C:\chromium.zip" -Force
-    
-    & "${env:ProgramFiles}\7-Zip\7z.exe" x "C:\chromium.zip" "-o$($chromiumPath)" -y
-    Remove-Item -Path "C:\chromium.zip" -Force
-
-    $shortCutPath = $env:USERPROFILE + "\Start Menu\Programs" + "\Chromium.lnk"
-    Remove-Item -Path $shortCutPath -Force -ErrorAction SilentlyContinue
-    $objShell = New-Object -ComObject ("WScript.Shell")
-    $objShortCut = $objShell.CreateShortcut($shortCutPath)
-    $objShortCut.TargetPath = "$chromiumPath\chrome-win\Chrome.exe"
-    $objShortCut.Save()
-}
-
-if ($true) {
-    Write-Host "Downloading FFmpeg..." -ForegroundColor Green
-    $ffmpegPath = "${env:ProgramFiles}\FFMPEG"
-    $downloadUri = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z"
-    
-    $downloadedFfmpeg = $env:USERPROFILE + "\Downloads\ffmpeg-git-full.7z"
-    Remove-Item $downloadedFfmpeg -ErrorAction SilentlyContinue
-    Start-Process "$env:ProgramFiles\Softdeluxe\Free Download Manager\fdm.exe" -PassThru "$downloadUri -force"
-        
-    while(-not $(Get-Item $downloadedFfmpeg -ErrorAction SilentlyContinue))
-    {
-        Write-Host "FFmpeg is still not downloaded!"
-        Start-Sleep -Seconds 5
-    }
-    
-    Move-Item $downloadedFfmpeg "C:\ffmpeg.7z" -Force
-    
-    & ${env:ProgramFiles}\7-Zip\7z.exe x "C:\ffmpeg.7z" "-o$($ffmpegPath)" -y
-    $subPath = $(Get-ChildItem -Path $ffmpegPath | Where-Object { $_.Name -like "ffmpeg*" } | Sort-Object Name -Descending | Select-Object -First 1).Name
-    $subPath = Join-Path -Path $ffmpegPath -ChildPath $subPath
-    $binPath = Join-Path -Path $subPath -ChildPath "bin"
-    Write-Host "Adding FFmpeg to PATH..." -ForegroundColor Green
-    [Environment]::SetEnvironmentVariable(
-        "Path",
-        [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";$binPath",
-        [EnvironmentVariableTarget]::Machine)
-    Remove-Item -Path "C:\ffmpeg.7z" -Force
-}
-if ($email.Contains('microsoft')) {
-    Install-IfNotInstalled Microsoft.VisualStudio.2022.Enterprise
-} else {
-    Install-IfNotInstalled Microsoft.VisualStudio.2022.Community
-}
-#>
     if (-not $(Get-Command git-lfs)) {
         winget install "GitHub.GitLFS" --source winget
     }
     else {
         Write-Host "Git LFS is already installed." -ForegroundColor Yellow
     }
-
-
-
+    
     Write-Host "-----------------------------" -ForegroundColor Green
     Write-Host "        PART 3  - Terminal    " -ForegroundColor Green
     Write-Host "-----------------------------" -ForegroundColor Green
@@ -323,31 +229,6 @@ if ($email.Contains('microsoft')) {
         [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\Program Files\Git\bin",
         [EnvironmentVariableTarget]::Machine)
 
-    <#没有aad没法登陆
-Write-Host "Enabling OneDrive silent sign in..." -ForegroundColor Green
-$HKLMregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive'##Path to HKLM keys
-$DiskSizeregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\DiskSpaceCheckThresholdMB'##Path to max disk size key
-if(!(Test-Path $HKLMregistryPath)){New-Item -Path $HKLMregistryPath -Force}
-if(!(Test-Path $DiskSizeregistryPath)){New-Item -Path $DiskSizeregistryPath -Force}
-Write-Host "Current AAD Tenant Id is $($aad.TenantId)"
-New-ItemProperty -Path $HKLMregistryPath -Name 'SilentAccountConfig' -Value '1' -PropertyType DWORD -Force | Out-Null ##Enable silent account configuration
-New-ItemProperty -Path $DiskSizeregistryPath -Name $aad.TenantId -Value '102400' -PropertyType DWORD -Force | Out-Null ##Set max OneDrive threshold before prompting
-Write-Host "Restarting OneDrive..." -ForegroundColor Yellow
-taskkill.exe /IM OneDrive.exe /F
-explorer "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
-explorer "$env:LOCALAPPDATA\Microsoft\OneDrive\OneDrive.exe"
-explorer "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
-
-$OneDrivePath = $null
-while ($null -eq $OneDrivePath -or -not $OneDrivePath.Contains("-")) {
-    Write-Host "OneDrive is still not started!"
-    # Wait till it finds my enterprise OneDrive folder.
-    Start-Sleep -Seconds 10
-    $OneDrivePath = $(Get-ChildItem -Path $HOME | Where-Object { $_.Name -like "OneDrive*" } | Sort-Object Name -Descending | Select-Object -First 1).FullName
-}
-Get-ChildItem $OneDrivePath | Format-Table -AutoSize
-#>
-    #执行自定义powershell
     Write-Host "Setting execution policy to remotesigned..." -ForegroundColor Green
     Set-ExecutionPolicy remotesigned
 
@@ -377,19 +258,19 @@ Get-ChildItem $OneDrivePath | Format-Table -AutoSize
     git config --global user.email $email
     git config --global user.name $name
     git config --global core.autocrlf true
-    <#
+    
     Write-Host "Linking back windows terminal configuration file..." -ForegroundColor Green
     $wtConfigPath = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
     $onedriveConfigwt = "$OneDrivePath\Storage\WT\settings.json"
     $_ = Get-Content $onedriveConfigwt # Ensure file is available.
     cmd /c "del `"$wtConfigPath`""
     cmd /c "mklink `"$wtConfigPath`" `"$onedriveConfigwt`""
-
+    
     Write-Host "Configuring windows terminal context menu..." -ForegroundColor Green
     git clone https://github.com/lextm/windowsterminal-shell.git "$HOME\temp"
     pwsh -command "$HOME\temp\install.ps1 mini"
     Remove-Item $HOME\temp -Force -Recurse -Confirm:$false
-    #>
+    
     <#
     Write-Host "-----------------------------" -ForegroundColor Green
     Write-Host "        PART 4  - SDK    " -ForegroundColor Green
@@ -446,10 +327,14 @@ Get-ChildItem $OneDrivePath | Format-Table -AutoSize
     Write-Host "Disabled Active Probing."
 
     Write-Host "Clearing start up..." -ForegroundColor Green
-    $startUp = $env:USERPROFILE + "\Start Menu\Programs\StartUp\*"
+    $startUp = $env:USERPROFILE + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\StartUp\*"
     Get-ChildItem $startUp
     Remove-Item -Path $startUp
     Get-ChildItem $startUp
+
+    Write-Host "Uninsatll some Software..." -ForegroundColor Green
+    winget uninstall MicrosoftWindows.Client.WebExperience_cw5n1h2txyewy
+    winget uninstall MicrosoftWindows.Teams
 
     Write-Host "Remove rubbish 3D objects..." -ForegroundColor Green
     Remove-Item 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}' -ErrorAction SilentlyContinue
@@ -458,7 +343,7 @@ Get-ChildItem $OneDrivePath | Format-Table -AutoSize
     Write-Host "Setting Power Policy to ultimate..." -ForegroundColor Green
     powercfg /s e9a42b02-d5df-448d-aa00-03f14749eb61
     powercfg /list
-
+    <#
     Write-Host "Enabling desktop icons..." -ForegroundColor Green
     cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
     cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f"
@@ -468,7 +353,7 @@ Get-ChildItem $OneDrivePath | Format-Table -AutoSize
     cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {645FF040-5081-101B-9F08-00AA002F954E} /t REG_DWORD /d 0 /f"
     cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu /v {F02C1A0D-BE21-4350-88B0-7367FC96EF3C} /t REG_DWORD /d 0 /f"
     cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {F02C1A0D-BE21-4350-88B0-7367FC96EF3C} /t REG_DWORD /d 0 /f"
-
+    #>
     $wallpaper = "$OneDrivePath\Pictures\Wallpaper\girl.jpg"
     if (Test-Path $wallpaper) {
         Write-Host "Setting wallpaper to $wallpaper..." -ForegroundColor Green
@@ -482,7 +367,7 @@ Get-ChildItem $OneDrivePath | Format-Table -AutoSize
     Write-Host "Monitor timeout set to 20."
 
     Write-Host "Enabling Chinese input method..." -ForegroundColor Green
-    $UserLanguageList = New-WinUserLanguageList -Language en-US
+    $UserLanguageList = New-WinUserLanguageList -Language zh-CN
     $UserLanguageList.Add("zh-CN")
     Set-WinUserLanguageList $UserLanguageList -Force
     $UserLanguageList | Format-Table -AutoSize
@@ -492,6 +377,8 @@ Get-ChildItem $OneDrivePath | Format-Table -AutoSize
 
     Write-Host "Disabling apps auto start..." -ForegroundColor Green
     cmd.exe /c "reg delete  HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v Wechat /f"
+    cmd.exe /c "reg delete  HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v YNote /f"
+    cmd.exe /c "reg delete  HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v cloudmusic /f"
     cmd.exe /c "reg delete  HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v `"Free Download Manager`" /f"
 
     Write-Host "Applying file explorer settings..." -ForegroundColor Green
